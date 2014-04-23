750e3c499d719de0100e19739903a38c5a25983a
Zoltan Horvath <zoltan@webkit.org>
2014-04-23 10:40:25 -0700

    Add bottom based triangle test.

--- tests/raster-tests.js
+++ tests/raster-tests.js
@@ -76,6 +76,28 @@ function register(mocha, expect) {
             it("line overlaps image's bottom part", function() { checkExclusionEdges(image, 75, 100, -10, 110)} );
         });
     });
+
+    describe("Image.left,rightExclusionEdge, bottom-base triangle", function() {
+        describe("shape-margin=0", function() {
+            var image = createRaster("./resources/bottom-based-triangle.png", 0, 0);
+            it("line equals image vertical extent", function() { checkExclusionEdges(image, 0, 100, 0, 100); });
+            it("line overlaps image vertical extent, above", function() { checkExclusionEdges(image, -100, 50, 24, 76); });
+            it("line overlaps image vertical extent, below", function() { checkExclusionEdges(image, 50, 150, 0, 100); });
+            it("line contains image vertical extent", function() { checkExclusionEdges(image, -100, 200, 0, 100)} );
+            it("line overlaps image's top part", function() { checkExclusionEdges(image, 0, 25, 37, 63)} );
+            it("line overlaps image's bottom part", function() { checkExclusionEdges(image, 75, 100, 0, 100)} );
+            it("line is above the image", function() { checkExclusionEdges(image, -50, -30, undefined, undefined)} );
+            it("line is below the image", function() { checkExclusionEdges(image, 101, 109, undefined, undefined)} );
+        });
+
+        describe("shape-margin=10", function() {
+            var image = createRaster("./resources/bottom-based-triangle.png", 0, 10);
+            it("line equals image vertical extent", function() { checkExclusionEdges(image, 0, 100, -10, 110); });
+            it("line equals image + shape-margin vertical extent", function() { checkExclusionEdges(image, -10, 110, -10, 110); });
+            it("line overlaps image's top part", function() { checkExclusionEdgesWithRound(image, 0, 25, 25, 75)} );
+            it("line overlaps image's bottom part", function() { checkExclusionEdges(image, 75, 100, -10, 110)} );
+        });
+    });
 }
 
 return {'register': register};
